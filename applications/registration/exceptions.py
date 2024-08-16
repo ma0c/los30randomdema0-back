ffb39c64dab543dad5e636621b77594e0bd5cdaf
Mauricio Collazos <mauricio@lextegrity.com>
2024-08-16 08:14:57 -0500

    Adding more exceptions

--- applications/registration/exceptions.py
+++ applications/registration/exceptions.py
@@ -4,9 +4,12 @@ from rest_framework import status
 
 class AttendeeAlreadyRegisteredException(APIException):
     status_code = status.HTTP_400_BAD_REQUEST
-    default_detail = 'Possible Attendee already exist, update the existing one instead of creating a new one.'
+    default_detail = {
+        "message": 'Possible Attendee already exist, update the existing one instead of creating a new one.'
+    }
     default_code = 1
 
     def __init__(self, registration):
+        self.default_detail["redirect"] = registration.slug
         super().__init__(self.default_detail)
         self.registration = registration
