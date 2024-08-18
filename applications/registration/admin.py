bd60ffa615f65037e938c0ad6e083d5778ba7ab8
Mauricio Collazos <mauricio@lextegrity.com>
2024-08-18 10:06:22 -0400

    Adding QR codes

--- applications/registration/admin.py
+++ applications/registration/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.utils.html import format_html
 
 from applications.registration.models import PossibleAttendees, Registration
+import qrcode
+from qrcode.image.svg import SvgPathImage
 
 
 # Register your models here.
@@ -11,9 +13,18 @@ class PossibleAttendeesAdmin(admin.ModelAdmin):
     def image_tag(self, obj):
         return format_html('<img src="{}" width=50 />'.format(obj.profile_pic.url)) if obj.profile_pic else None
 
-    image_tag.short_description = 'Image'
+    def qr_code(self, obj):
+        img = qrcode.make(obj.slug, image_factory=SvgPathImage)
+        # img = img.make_image(fill_color="white")
+        img_string = img.to_string(encoding='unicode')
+        return format_html(img_string)
 
-    list_display = ('name', 'phone', 'slug', 'image_tag')
+    image_tag.short_description = 'Image'
+    qr_code.short_description = 'QR Code'
+
+
+
+    list_display = ('name', 'phone', 'slug', 'image_tag', 'qr_code')
     search_fields = ('name', 'phone')
 
 
